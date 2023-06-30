7e5ee5963e1360518114b8f00c2bf39b2db2f518
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-06-29 23:07:58 -0300

    added title
    title component added

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import {
+  Grid,
+  ThemeProvider,
+  Typography,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material";
 const Title = () => {
-  return <div>Title</div>;
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
+
+  return (
+    <Grid
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      marginX={2}
+    >
+      <ThemeProvider theme={theme}>
+        <Typography variant="h1" className="overflow-hidden text-white">
+          Just QR 👌
+        </Typography>
+        <Typography
+          variant="body2"
+          className="text-white
+"
+        >
+          Easily transform any text or link into an scaneable QR code
+        </Typography>
+      </ThemeProvider>
+    </Grid>
+  );
 };
 
 export default Title;
